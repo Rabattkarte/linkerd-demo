847ddae18bfca6a99f154620be7c6042ef0e26ed
Nils Fahldieck <nils.fahldieck@gmail.com>
2022-07-20 15:14:04 +0200

    Streamline setup

--- 00_setup_cluster_and_linkerd.sh
+++ 00_setup_cluster_and_linkerd.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-set -x
-
-# Check for all necessary tools
-function check_tools {
-  for tool in "$@"; do
-    if ! command -v "$tool" &>/dev/null; then
-      printf "'%s' could not be found. Aborting.\n" "$tool"
-      exit
-    fi
-  done
-}
 
 function die_on_failure {
   "$@"
@@ -21,43 +10,39 @@ function die_on_failure {
   return $status
 }
 
-check_tools minikube linkerd
-
 # Variables
 MK_PROFILE_NAME="${MK_PROFILE_NAME:-linkerd}"
 
 # Create a minikube cluster & select it as default profile
-minikube start --memory=8g --cpus=4 --profile "$MK_PROFILE_NAME"
+minikube start \
+  --memory=8g \
+  --cpus=4 \
+  --profile "$MK_PROFILE_NAME"
 minikube profile "$MK_PROFILE_NAME"
 
 # Run linkerd pre-checks
 die_on_failure linkerd check --pre
 
 # Install linkerd
-linkerd install | kubectl apply -f -
+linkerd install --set proxyInit.runAsRoot=true |
+  kubectl apply --filename -
 
 # Checking the linkerd install
 die_on_failure linkerd check
 
-# Now install our emojivoto demo app
-curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/emojivoto.yml |
-  kubectl apply -f -
+# install the on-cluster metrics stack
+linkerd viz install |
+  kubectl apply --filename -
 
-# Inject linkerd to all emojivoto deployments
-kubectl get -n emojivoto deploy -o yaml |
-  linkerd inject - |
-  kubectl apply -f -
+# Install the emojivoto demo app with injected linkerd
+linkerd inject https://run.linkerd.io/emojivoto.yml |
+  kubectl apply --filename -
 
 # Let's check the proxy/sidecars
-die_on_failure linkerd check --proxy -n emojivoto
+die_on_failure linkerd check --proxy --namespace emojivoto
 
-# install the on-cluster metrics stack
-linkerd viz install | kubectl apply -f -
-
+# Final check
 die_on_failure linkerd check
 
-# Forward the viz dashboard
-linkerd viz dashboard &
-
-# Expose the web app
-kubectl -n emojivoto port-forward svc/web-svc 8080:80 &
+# Make the app & dashboard available
+./10_run_dashboard_and_app.sh
