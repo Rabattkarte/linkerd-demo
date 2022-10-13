5104f766ffe90f05f9eb1ce4b9fbc75d96c12f93
Nils Fahldieck <nils.fahldieck@gmail.com>
2022-10-13 17:59:29 +0200

    Add clean-up script

---
+++ 20_delete_minikube_cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+MK_PROFILE_NAME="${MK_PROFILE_NAME:-linkerd}"
+
+# Ask the user for confirmation prior to deletion
+read -p "Do you want to delete the minikube cluster '$MK_PROFILE_NAME'? [Yy|Nn]: " -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  # Delete the minikube cluster and all resources in it
+  minikube delete -p "$MK_PROFILE_NAME"
+fi
+
+# Return the information about our local minikube profiles
+minikube profile list
