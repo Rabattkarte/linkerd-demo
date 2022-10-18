4bbd135f07d9cc5e6cde93cf201403fabf3d3e97
Nils Fahldieck <nils.fahldieck@gmail.com>
2022-10-18 21:09:26 +0200

    Make default case for deletion script

--- 90_delete_minikube_cluster.sh
+++ 90_delete_minikube_cluster.sh
@@ -8,7 +8,8 @@ read -p "Do you want to delete the minikube cluster '$MK_PROFILE_NAME'? [Yy|Nn]:
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   # Delete the minikube cluster and all resources in it
   minikube delete -p "$MK_PROFILE_NAME"
+  # Return the information about our local minikube profiles
+  minikube profile list
+else
+  echo "Will do nothing."
 fi
-
-# Return the information about our local minikube profiles
-minikube profile list
